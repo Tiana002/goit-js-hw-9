c72a30b3b08d9c899bb366a1e27f50f0806e0369
Tetiana Blinova <tiana002@gmail.com>
2023-04-13 22:22:02 +0300

    task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const dataStart = document.querySelector('[data-start]');
+const dataStop = document.querySelector('[data-stop]');
+const bodyColor = document.querySelector('body');
+
+let timerId = null;
+
+ function getRandomHexColor() {
+     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+dataStart.addEventListener('click', onStart);
+dataStop.addEventListener('click', onStop);
+
+function getBgColor() {
+    bodyColor.style.backgroundColor =getRandomHexColor();
+}
+function onStart() {
+    timerId = setInterval(getBgColor, 1000);
+    dataStart.toggleAttribute('disabled');
+}
+
+function onStop() {
+    clearInterval(timerId);
+    dataStart.removeAttribute('disabled');
+}
 
